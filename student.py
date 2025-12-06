50cc0dc46bb8deb3b518d577e123832c319cd7e3
KenulaNimhan <kenulanimhan@gmail.com>
2025-12-06 14:14:52 +0530

    add validator setters

--- student.py
+++ student.py
@@ -1,12 +1,39 @@
+import re
+
+# define validator constants
+EMAIL_REGEX = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
+
 class Student:
 
     # define constructor
-    def __init__(self, name, age, email, password):
+    def __init__(self, name, age=0, email="", password=""):
         self.studentId = ""
         self.name = name
         self.age = age
         self.email = email
         self.password = password
+
+    # setters with validators
+    def set_studentId(self, id):
+        self.studentId = id
+
+    def set_age(self, age):
+        if (age>=18 and age<=40):
+            self.age = age
+        else:
+            raise ValueError("Age is invalid")
+
+    def set_email(self, email):
+        if (re.match(EMAIL_REGEX,email)):
+            self.email = email
+        else:
+            raise ValueError("Email is invalid")
+
+    def set_password(self, password):
+        if (len(password)>5):
+            self.password = password
+        else:
+            raise ValueError("Password is invalid")
 
     def __str__(self) -> str:
         return f'Student Details\nname: {self.name}\nage: {self.age}'
