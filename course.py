f487ad6127dd4c67b641cbfddc0721464c86dd30
KenulaNimhan <kenulanimhan@gmail.com>
2025-12-07 12:51:55 +0530

    remove id setter

--- course.py
+++ course.py
@@ -1,14 +1,13 @@
 class Course:
 
     # define constructor
-    def __init__(self, subject, tutor):
-        self.courseId = None
+    def __init__(self, subject, tutor, courseId=None):
+        self.courseId = courseId
         self.subject = subject
         self.tutor = tutor
 
-    # setter methods
-    def set_courseId(self, id):
-        self.courseId = id
-
     def __str__(self) -> str:
         return f'Course Details\nsubject: {self.subject}\ntutor: {self.tutor}'
+
+    def __repr__(self) -> str:
+        return self.__str__()
