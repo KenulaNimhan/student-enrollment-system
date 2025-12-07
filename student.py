65aeae780ba3c0b0d68ab824ff65824a90e6217b
KenulaNimhan <kenulanimhan@gmail.com>
2025-12-07 16:04:35 +0530

    minor refactor

--- student.py
+++ student.py
@@ -6,8 +6,8 @@ EMAIL_REGEX = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
 class Student:
 
     # define constructor
-    def __init__(self, name, age=0, email="", password=""):
-        self.studentId = None
+    def __init__(self, name, age=None, email=None, password=None, studentId=None):
+        self.studentId = studentId
         self.name = name
         self.age = age
         self.email = email
