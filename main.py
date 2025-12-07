35db0ea52cb5b36fc6972dd3fd377fc820f3c137
KenulaNimhan <kenulanimhan@gmail.com>
2025-12-07 16:04:23 +0530

    add post methods

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import database
+from student import Student
+from requestDTO.register_request import RegisterReq
+from requestDTO.course_request import CourseReq
 
 app = FastAPI()
 
@@ -9,20 +12,53 @@ def root():
 
 # GET
 
-@app.get("/student-details")
-def get_details_of_student(student_id):
-    return database.fetch_details_of_student(student_id)
-
-@app.get("/course-students/{course_id}")
-def get_students_enrolled_in_course(course_id):
-    return database.fetch_names_of_enrolled_students(course_id)
-
 @app.get("/all-courses")
 def get_all_courses():
     return database.fetch_all_courses()
 
+    # expects a query parameter
+
+@app.get("/student-details")
+def get_details_of_student(student_id: int):
+    return database.fetch_details_of_student(student_id)
+
+@app.get("/course-students")
+def get_students_enrolled_in_course(course_id: int):
+    return database.fetch_names_of_enrolled_students(course_id)
+
 @app.get("/enrolled-courses")
-def get_enrolled_courses(student_id):
+def get_enrolled_courses(student_id: int):
     return database.fetch_enrolled_courses_of_student(student_id)
 
 # POST
+
+@app.post("/register-student")
+def register_student(request: RegisterReq):
+    new_student = Student(request.name)
+    try:
+        new_student.set_age(request.age)
+        new_student.set_email(request.email)
+        new_student.set_password(request.password)
+        database.register_student(new_student)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return new_student
+
+@app.post("/enroll")
+def enroll(request: CourseReq):
+    try:
+        database.enroll(request.student_id, request.course_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# DELETE
+
+@app.delete("/disenroll")
+def remove_student_from_course(request: CourseReq):
+    try:
+        database.disenroll_from_course(request.student_id, request.course_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
