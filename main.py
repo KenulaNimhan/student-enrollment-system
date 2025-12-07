54c9c6b8e25f5e9d39f6fc21dc24e585c937104d
KenulaNimhan <kenulanimhan@gmail.com>
2025-12-07 13:07:55 +0530

    update api url

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ def root():
 
 # GET
 
-@app.get("/student/details")
+@app.get("/student-details")
 def get_details_of_student(student_id):
     return database.fetch_details_of_student(student_id)
 
-@app.get("/students/enrolled-in-course/{course_id}")
+@app.get("/course-students/{course_id}")
 def get_students_enrolled_in_course(course_id):
     return database.fetch_names_of_enrolled_students(course_id)
 
-
-@app.get("/courses")
+@app.get("/all-courses")
 def get_all_courses():
     return database.fetch_all_courses()
 
-@app.get("/courses/enrolled-by-student")
+@app.get("/enrolled-courses")
 def get_enrolled_courses(student_id):
     return database.fetch_enrolled_courses_of_student(student_id)
 
